e7bd935e012663dd4b55d92f6bd74702eccd36ec
Hammad Ali <hammadali78056@gmail.com>
2026-04-27 10:19:37 +0500

    Initial Commit

---
+++ AMS_System/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMS_System
+//
+//  Created by Hammad Ali on 27/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
